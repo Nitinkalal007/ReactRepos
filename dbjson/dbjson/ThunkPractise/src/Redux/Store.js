755acc171af6be89df0a91328dc843a290c94ed1
Nitinkalal007 <nitinkalal01@gmail.com>
2024-12-13 19:46:23 +0530

    Delete dbjson directory

--- dbjson/dbjson/ThunkPractise/src/Redux/Store.js
+++
@@ -1,9 +0,0 @@
-// Shree Ganeshay namah 
-
-import { configureStore } from "@reduxjs/toolkit";
-import userslice from './userSlice.js'
-export const Store = configureStore({
-    reducer:{
-        user:userslice
-    }
-})
